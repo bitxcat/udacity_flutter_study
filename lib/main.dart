472b7ec93b4be845ab03827b020dc6923dcec424
bitxcat <bitxcat@gmail.com>
2019-01-14 09:25:48 +0800

    03-category-route

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,8 @@ import 'package:flutter/material.dart';
 // You can use a relative import, i.e. `import 'category.dart;'` or
 // a package import, as shown below.
 // More details at http://dart-lang.github.io/linter/lints/avoid_relative_lib_imports.html
-import 'package:udacity_flutter_study/category.dart';
-
-const _categoryName = 'Alarm';
-const _categoryIcon = Icons.alarm_on; // Icons 
-const _categoryColor = Colors.red;
+//import 'package:udacity_flutter_study/category.dart';
+import 'package:udacity_flutter_study/category_route.dart';
 
 /// The function that is called when main.dart is run.
 /// 主结构:main > runapp(widget(widget(widget)))
@@ -20,20 +17,11 @@ void main() {
 /// Currently, we just show one widget in our app.
 class UnitConverterApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context) { // 都要实现一个 build
+    return MaterialApp( // MaterialApp 
       debugShowCheckedModeBanner: false,
-      title: 'Unit Converter',
-      home: Scaffold(
-        backgroundColor: Colors.green[100],
-        body: Center(
-          child: Category(
-            name: _categoryName,
-            color: _categoryColor,
-            iconLocation: _categoryIcon,
-          ),
-        ),
-      ),
+      title: 'Unit Converter', // 为毛没看见 title
+      home: CategoryRoute(),
     );
   }
 }
